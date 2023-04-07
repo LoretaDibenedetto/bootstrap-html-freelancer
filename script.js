d36469e38ddc9dfb6a27ca3a7be7d6c3cb72d548
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-07 10:13:02 +0200

    add select type of work

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 
+let disocuntCodes = ["YHDNU32", "JANJC63" , "PWKCN25", "POCIE24", "SJDPO96" ];
+
+
 function validateForm(event){
    
     event.preventDefault(event);
 
     console.log("FUNZIONAAAA!!!");
 
+    
 
+    let selectedTypeOfWork = document.getElementById("work-type").value;
 
+    console.log(selectedTypeOfWork);
 
+    let Price = 0;
 
+    switch(selectedTypeOfWork){
+        case "backend":
+            Price += 20.50;
+            break;
+        case "frontend":
+            Price += 15.30;
+            break; 
+        case "analize project":
+            
+            Price += 33.60;
+            break;
+        
+    };
 
+    document.getElementById("finalPrice").innerHTML = Price.toFixed(2);
 
 
 
